4b8c417dbacc5b1df70aca645f0d794ce341e778
Ryan Rodriguez <ryancode2321@gmail.com>
2025-12-05 21:08:26 -0600

    mid

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 import os
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "[*]"
+    ],
+    allow_origin_regex="https://.*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
